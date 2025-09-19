303ded5938a59ab6cf77a17d686b19752e32d47a
ali <ali@gmail.com>
2025-09-19 18:54:46 +0200

    Add 14-InvertedLetterPattern.cpp solution

---
+++ 14-InvertedLetterPattern.cpp
@@ -0,0 +1,34 @@
+using namespace std;
+
+int ReadPositiveNumber(string Message)
+{
+    int number = 0;
+    do
+    {
+        cout << Message << endl;
+        cin >> number;
+    } while (number <= 0);
+
+    return number;
+
+}
+
+void getIvertedPatterOfNumber(int number) {
+
+    cout << endl;
+
+    for(int i = 65 + number - 1; i >= 65; i--){
+
+        for (int j = 1; j <= number - ((65 + number - 1) - i); j++) {
+            cout << char(i);
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    getIvertedPatterOfNumber(ReadPositiveNumber("Plaese enter your number"));
+
+
+    return 0;
+}

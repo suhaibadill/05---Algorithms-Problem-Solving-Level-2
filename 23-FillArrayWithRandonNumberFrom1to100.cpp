246f6e4c2e076db1b929996f9a185f7c84cbb908
ali <ali@gmail.com>
2025-09-19 19:11:24 +0200

    Add 23-FillArrayWithRandonNumberFrom1to100.cpp solution

---
+++ 23-FillArrayWithRandonNumberFrom1to100.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int randomNumber(int from, int to) {
+
+    int randomNumber = rand() % (to - from + 1) + from;
+
+    return randomNumber;
+}
+
+void getArrayElements(int arr[100], int& arrLength) {
+
+    cout << "Enter how many elments do you want?\n";
+    cin >> arrLength;
+
+    for (int i = 0; i < arrLength; i++) {
+        arr[i] = randomNumber(1, 100);
+        
+    }
+}
+
+void printArrayElements(int arr[100], int arrLength) {
+
+    for (int i = 0; i < arrLength; i++) {
+        cout << arr[i] << " ";
+
+    }
+    cout << endl;
+}
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    int arr[100],  arrLength;
+
+    getArrayElements(arr, arrLength);
+
+    cout << "\nArray Elements: ";
+    printArrayElements(arr, arrLength);
+
+    return 0;
+}

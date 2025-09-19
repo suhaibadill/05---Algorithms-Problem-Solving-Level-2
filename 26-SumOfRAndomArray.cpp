9db95e24bfe56f63b1d519b5cc36a45432d6c4cd
ali <ali@gmail.com>
2025-09-19 19:16:29 +0200

    Add 26-SumOfRAndomArray.cpp solution

---
+++ 26-SumOfRAndomArray.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int randomNumber(int from, int to) {
+
+    int randomNumber = rand() % (to - from + 1) + from;
+
+    return randomNumber;
+}
+
+void getArrayElements(int arr[100], int& arrLength) {
+
+    cout << "Enter how many elments do you want?\n";
+    cin >> arrLength;
+
+    for (int i = 0; i < arrLength; i++) {
+        arr[i] = randomNumber(1, 100);
+
+    }
+}
+
+void printArrayElements(int arr[100], int arrLength) {
+
+    for (int i = 0; i < arrLength; i++) {
+        cout << arr[i] << " ";
+
+    }
+    cout << endl;
+}
+
+int sumNumberOnTheArray(int arr[100], int arrLength) {
+
+    int sum = 0;
+    for (int i = 0; i < arrLength; i++) {
+
+        sum += arr[i];
+    }
+    return sum;
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    int arr[100], arrLength;
+
+    getArrayElements(arr, arrLength);
+
+    cout << "\nArray Elements: ";
+    printArrayElements(arr, arrLength);
+    cout << "Min Number is: " << sumNumberOnTheArray(arr, arrLength) << endl;
+
+    return 0;
+}
